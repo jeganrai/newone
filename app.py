a97a41611cd98b4f9005a87f3b62de307178ceac
jeganrai <jeganrai10@gmail.com>
2019-08-07 19:20:40 +0530

    fiest

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask,render_template
+import requests
+import json
+
+app=Flask(__name__)
+@app.route('/')
+def index():
+    r=requests.get("http://api.icndb.com/jokes/random")
+    data=json.loads(r.text)
+    joke=data['value']['joke']
+    return render_template('index.html',joke=joke)
+
+app.run(debug=True)
