858d595eb56fe1a74fb92c4c93da399e413364de
jeganrai <jeganrai10@gmail.com>
2019-08-09 17:11:59 +0530

    jegan

--- appp.py
+++ appp.py
@@ -6,6 +6,11 @@ app=Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/ram/')
+def iram():
+    return "hello"
+
+
 
 if __name__ == '__main__':
     app.run()
